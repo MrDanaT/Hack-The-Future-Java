5f291aedb4cb38b03eb718169095097b97b325fb
DaneelsT <43535507+DaneelsT@users.noreply.github.com>
2020-11-24 09:57:18 +0100

    Initial

---
+++ htf/src/main/java/be/ap/ti/htf/HtfApplication.java
@@ -0,0 +1,13 @@
+package be.ap.ti.htf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtfApplication.class, args);
+	}
+
+}
---
+++ htf/src/test/java/be/ap/ti/htf/HtfApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ap.ti.htf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
